861181b1db8298c4014ac00112af21fe87bd5f40
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-08-11 03:14:21 +0530

    chore: updated JSON middleware

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  if (Buffer.isBuffer(req.body)) {
+  if (req.body && Buffer.isBuffer(req.body)) {
     req.rawBody = req.body;
     req.body = req.body.toString();
     req.body = JSON.parse(req.body);
