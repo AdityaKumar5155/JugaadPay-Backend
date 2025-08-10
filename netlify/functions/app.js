54fb11a82df3cf49ffc18a2da7e4c6087fa3d054
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-08-11 03:24:59 +0530

    chore: updated JSON middleware

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -5,28 +6,27 @@ const cors = require('cors');
 const helmet = require('helmet');
 const serverless = require('serverless-http');
 
-// Middleware setup
-app.use(cors());
-app.use(helmet());
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  if (req.body && Buffer.isBuffer(req.body)) {
-    try{
-      req.rawBody = req.body;
-      req.body = req.body.toString();
-      req.body = JSON.parse(req.body);
-    } catch (error) {
-      console.error('Error parsing body:', error);
-    }
-  }
-  next();
-});
 
 const authRoutes = require('../../routes/auth.routes');
 const personRoutes = require('../../routes/person.routes');
 const userRoutes = require('../../routes/user.routes');
 const transactionRoutes = require('../../routes/transaction.routes');
 const transactionPayeesRoutes = require('../../routes/transaction_payees.routes');
+
+// Middleware setup
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  if (Buffer.isBuffer(req.body)) {
+    try {
+      req.body = JSON.parse(req.body.toString());
+    } catch (err) {
+      console.error("Failed to parse buffer body:", err.message);
+    }
+  }
+  next();
+});
 
 app.use('/auth', authRoutes);
 app.use('/persons', personRoutes);
